84911d1701a39a6e10b6609a75f7740be784b2d1
PC <PC@MaxStreet-PC>
2024-06-13 17:20:15 -0300

    Inicio de alumno (Inicio de Rama)

---
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.model;
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Data
+@Component
+@Entity
+public class Alumno {
+	@Id
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private String fechaNacimiento;
+	private String domicilio;
+	private String libretaUniversitaria;
+	private boolean estado;
+}
