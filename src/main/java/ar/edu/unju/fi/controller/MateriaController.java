a260ed643f092345cbe7327fbfbf41aea94c1b3f
Nahuel <Nahuel@DESKTOP-NAHUEL>
2024-06-18 12:58:13 -0300

    Avanze de Materia en Repository, Service y Cambios sobre el Controller| Vilte Daniel Nahuel

--- src/main/java/ar/edu/unju/fi/controller/MateriaController.java
+++ src/main/java/ar/edu/unju/fi/controller/MateriaController.java
@@ -1,5 +1,5 @@
 package ar.edu.unju.fi.controller;
-/*
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,23 +8,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.unju.fi.collections.ListadoCarreras;
-import ar.edu.unju.fi.collections.ListadoDocentes;
-import ar.edu.unju.fi.collections.ListadoMaterias;
 import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.service.MateriaService;
 
 @Controller
 public class MateriaController {
+	
 	@Autowired
 	Materia nuevaMateria;
+	
+	@Autowired
+	MateriaService materiaService;
 	
 	@GetMapping("/formularioMaterias")
 	public ModelAndView getFormMateria() {
 		ModelAndView modelView = new ModelAndView("formMateria");
 		modelView.addObject("nuevaMateria", nuevaMateria);	
-		modelView.addObject("listadoDocentes", ListadoDocentes.listarDocentes());
-		modelView.addObject("listadoCarreras", ListadoCarreras.listarCarreras());
+		//modelView.addObject("listadoDocentes", ListadoDocentes.listarDocentes());
+		//modelView.addObject("listadoCarreras", ListadoCarreras.listarCarreras());
 		modelView.addObject("flag", false);
+		modelView.addObject("band", false);
 		return modelView;
 	}
 	
@@ -32,17 +35,20 @@ public class MateriaController {
 	public ModelAndView getFormListaMateria() {
 		//vista formCarrera.html
 		ModelAndView modelView = new ModelAndView("listaDeMaterias");
-		modelView.addObject("listadoMaterias", ListadoMaterias.listarMaterias());	
+		modelView.addObject("listadoMaterias", materiaService.mostrarMaterias());	
 		return modelView;	
 	}
 	
 	@PostMapping("/guardarMateria")
 	public ModelAndView saveMateria(@ModelAttribute("nuevaMateria") Materia materiaParaGuardar) {
-		materiaParaGuardar.setDocente(ListadoDocentes.buscarDocentePorLegajo(materiaParaGuardar.getDocente().getLegajo()));
-		materiaParaGuardar.setCarrera(ListadoCarreras.buscarCarreraPorCodigo(materiaParaGuardar.getCarrera().getCodigo()));
-		ListadoMaterias.agregarMateria(materiaParaGuardar);
+		//materiaParaGuardar.setDocente(ListadoDocentes.buscarDocentePorLegajo(materiaParaGuardar.getDocente().getLegajo()));
+		//materiaParaGuardar.setCarrera(ListadoCarreras.buscarCarreraPorCodigo(materiaParaGuardar.getCarrera().getCodigo()));
+		//ListadoMaterias.agregarMateria(materiaParaGuardar);
+		materiaService.guardarMateria(materiaParaGuardar);
+		
 		ModelAndView modelView = new ModelAndView("listaDeMaterias");
-		modelView.addObject("listadoMaterias", ListadoMaterias.listarMaterias());	
+		//modelView.addObject("listadoMaterias", ListadoMaterias.listarMaterias());	
+		modelView.addObject("listadoMaterias", materiaService.mostrarMaterias());	
 		
 		return modelView;		
 	}
@@ -50,33 +56,37 @@ public class MateriaController {
 	@GetMapping("/borrarMateria/{codigo}")
 	public ModelAndView deleteMateriaDelListado(@PathVariable(name="codigo") String codigo) {
 		//borrar
-		ListadoMaterias.eliminarMateria(codigo);
+		//ListadoMaterias.eliminarMateria(codigo);
+		materiaService.borrarMateria(codigo);
+		//Mostrar listado
 		ModelAndView modelView = new ModelAndView("listaDeMaterias");
-		modelView.addObject("listadoMaterias", ListadoMaterias.listarMaterias());	
+		modelView.addObject("listadoMaterias", materiaService.mostrarMaterias());	
 		
 		return modelView;		
 		}
 	
 	@GetMapping("/modificarMateria/{codigo}")
     public ModelAndView getFormModificarMateria(@PathVariable(name="codigo") String codigo) {
-        Materia materia = ListadoMaterias.buscarMateriaPorCodigo(codigo);
-        System.out.println(materia.getCodigo());
+        //Materia materia = ListadoMaterias.buscarMateriaPorCodigo(codigo);
+        Materia materia = materiaService.buscarMateria(codigo);
+		System.out.println(materia.getCodigo());
         ModelAndView modelView = new ModelAndView("formMateria");
         modelView.addObject("nuevaMateria", materia);
-        modelView.addObject("listadoDocentes", ListadoDocentes.listarDocentes());
-		modelView.addObject("listadoCarreras", ListadoCarreras.listarCarreras());
+        //modelView.addObject("listadoDocentes", ListadoDocentes.listarDocentes());
+		//modelView.addObject("listadoCarreras", ListadoCarreras.listarCarreras());
         modelView.addObject("flag", true);
         return modelView;
     }
 
     @PostMapping("/modificarMateria")
     public ModelAndView modificarMateria(@ModelAttribute("nuevaMateria") Materia materiaModificada) {
-		materiaModificada.setDocente(ListadoDocentes.buscarDocentePorLegajo(materiaModificada.getDocente().getLegajo()));
-		materiaModificada.setCarrera(ListadoCarreras.buscarCarreraPorCodigo(materiaModificada.getCarrera().getCodigo()));
-        ListadoMaterias.modificarMateria(materiaModificada);
-        ModelAndView modelView = new ModelAndView("listaDeMaterias");
-        modelView.addObject("listadoMaterias", ListadoMaterias.listarMaterias());
+		//materiaModificada.setDocente(ListadoDocentes.buscarDocentePorLegajo(materiaModificada.getDocente().getLegajo()));
+		//materiaModificada.setCarrera(ListadoCarreras.buscarCarreraPorCodigo(materiaModificada.getCarrera().getCodigo()));
+        //ListadoMaterias.modificarMateria(materiaModificada);
+        materiaService.modificarMateria(materiaModificada);
+    	ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("listadoMaterias", materiaService.mostrarMaterias());
         return modelView;
     }
+    
 }
-*/
---
+++ src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImp.java
@@ -0,0 +1,62 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.repository.MateriaRepository;
+import ar.edu.unju.fi.service.MateriaService;
+
+@Service
+public class MateriaServiceImp implements MateriaService{
+	
+	@Autowired
+	MateriaRepository materiaRepository;
+	
+	@Override
+	public void guardarMateria(Materia materia) {
+		// TODO Auto-generated method stub
+		materia.setEstado(true);
+		materiaRepository.save(materia);
+	}
+
+	@Override
+	public List<Materia> mostrarMaterias() {
+		// TODO Auto-generated method stub
+		
+		return materiaRepository.findMateriaByEstado(true);
+	}
+
+	@Override
+	public void borrarMateria(String codigo) {
+		List<Materia> carreras = materiaRepository.findAll();
+		carreras.forEach(carrera -> {
+			if(carrera.getCodigo().equals(codigo)) {
+				carrera.setEstado(false);
+				materiaRepository.save(carrera);
+			}
+		});
+	}
+
+	@Override
+	public Materia buscarMateria(String codigo) {
+		// TODO Auto-generated method stub
+		List<Materia> todasLasMaterias = materiaRepository.findAll();
+		for (Materia materias : todasLasMaterias) {
+			if(materias.getCodigo().equals(codigo)) {
+				return materias;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void modificarMateria(Materia materiaModificada) {
+		// TODO Auto-generated method stub
+		materiaModificada.setEstado(true);
+		materiaRepository.save(materiaModificada);
+	}
+
+}
